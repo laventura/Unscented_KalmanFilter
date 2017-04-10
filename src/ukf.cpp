f1c6b10d85ddf4dfc6b237e090c3ade27b6c7fe4
Atul Acharya <atul.acharya@gmail.com>
2017-04-10 01:31:15 -0700

    tuning params; working on both datasets

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -25,10 +25,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_      = 0.63;   // 3;    // was 30
+  std_a_      = 1.5; // orig 30; tested with 0.6 - 3
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_  = 0.6;  // 1.5; was 30
+  std_yawdd_  = 0.6;  // orig 30; test with 1.5 - 3
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_  = 0.15;
@@ -43,7 +43,7 @@ UKF::UKF() {
   std_radphi_ = 0.03;
 
   // Radar measurement noise standard deviation radius change in m/s
-  std_radrd_  = 0.3;  // 0.3, 0.4
+  std_radrd_  = 0.3;  
 
   /**
   TODO:
